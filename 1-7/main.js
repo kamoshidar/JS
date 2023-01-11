3c5d5cb0a3db1ef0b5ace052fd80eb570074ba03
kamoshidar <108622375+kamoshidar@users.noreply.github.com>
2023-01-11 22:35:23 +0900

    Create main.js

---
+++ 1-7/main.js
@@ -0,0 +1,14 @@
+class Taiyaki{
+    constructor(name){
+        this.name = name;
+    }
+    nakami(){
+        console.log(`中身は${this.name}です。`);
+    }
+}
+let anko = new Taiyaki('あんこ');
+anko.nakami();
+let cream = new Taiyaki('クリーム');
+cream.nakami();
+let cheese = new Taiyaki('チーズ');
+cheese.nakami();
